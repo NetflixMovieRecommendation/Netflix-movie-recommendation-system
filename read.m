a3ba0025bf46f98dc12ba374c6945d5f860129c0
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-04-05 11:57:07 -0400

    full outer join function

--- read.m
+++ read.m
@@ -8,18 +8,16 @@
     for i = 1:fileLen
         fullLocation = strcat(path,fileNames(i).name);
         file = fopen(fullLocation);
-        % skip ythe first row
+        % skip the first row
         textscan(file, '%*s', 1);
         % userID, rating, date omitted here
         dataCell = textscan(file, '%u32 %u32 %*[^\n]','Delimiter',',');
         % cell merge by columns
         tempData = [dataCell{1} dataCell{2}];
-        if  i == 1
-            s = [s; size(dataCell{1},1)];
-        else
-            s = [s; s(i-1)+size(dataCell{1},1)];
-        end
-        data = [data; tempData];
+       
+       
+ 
+        
         fclose(file);
     end
     
